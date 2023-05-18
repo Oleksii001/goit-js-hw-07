5d5a8de9fd3e4dc55638d46cbb530f5d95f9d783
Oleksii001 <oleksii.burlaka@gmail.com>
2023-05-18 22:19:59 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,34 +10,6 @@ const createMarkup = galleryItems.map(({ preview, original, description }) => `<
 </li>`);
 
 galleryRef.insertAdjacentHTML("beforeend", createMarkup.join(''));
-galleryRef.addEventListener('click', onClick);
-
-let instance;
-
-function onClick(evt) {
-    evt.preventDefault();
-    if (evt.target === evt.currentTarget) {
-        return;
-    }
-
-    instance = basicLightbox.create(`<img src="${evt.target.dataset.source}" width="800" height="600">`, {
-        onClose: () => {
-            window.removeEventListener("keydown", handlePressEscKey);
-        },
-        onShow: () => {
-            window.addEventListener("keydown", handlePressEscKey);
-        },
-    });
-
-    instance.show();
-}
-
-function handlePressEscKey(evt) {
-    if (evt.code !== "Escape") {
-        return;
-    }
-    instance.close();
-}
 
 
 const gallery = new SimpleLightbox('.gallery a', {
